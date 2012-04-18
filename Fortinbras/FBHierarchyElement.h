404186171a0d6d157dbdbcab8ba509c013b7dd2e
Torsten Schlumm <torsten@zuse.cbs.mpg.de>
2012-04-18 20:20:44 +0200

    hierarchy element basics

--- Fortinbras/FBHierarchyElement.h
+++ Fortinbras/FBHierarchyElement.h
@@ -8,6 +8,39 @@
 
 #import <Foundation/Foundation.h>
 
-@interface FBHierarchyElement : NSObject
+@interface FBHierarchyElement : NSObject 
+
+    
+@property (readonly) uuid_t   *uuid;
+@property (readonly) NSString *name;
+@property (readonly) NSString *comment;
+
+@property (readonly) NSInteger state;
+
+@property (readonly) NSMutableDictionary *properties;
+
+@property (copy) FBHierarchyElement  *parent;
+@property (readonly) NSMutableArray   *children;
+
+
+extern NSString * const FB_ELEMENT_PROPERTY_NAME;
+extern NSString * const FB_ELEMENT_PROPERTY_COMMENT;
+extern NSString * const FB_ELEMENT_PROPERTY_STATE;
+
+extern NSInteger  const FB_ELEMENT_STATE_UNKNOWN;
+extern NSInteger  const FB_ELEMENT_STATE_ERROR;
+extern NSInteger  const FB_ELEMENT_STATE_NOT_CONFIGURED;
+extern NSInteger  const FB_ELEMENT_STATE_READY;
+extern NSInteger  const FB_ELEMENT_STATE_RUNNING;
+extern NSInteger  const FB_ELEMENT_STATE_FINISHED;
+
+
+-(id)init;
+-(id)initWithName: (NSString*)name;
+-(id)initWithNameAndComment: (NSString*)name comment: (NSString*)comment;
+
+-(NSString*)description;
+
 
 @end
+
